1637c24889c8f6323a4052db2eccf197f8d5beba
Subodh Singh <sksingh329@gmail.com>
2026-01-11 21:07:11 +0530

    assertion for create user test

--- tests/test_gorest_crud.py
+++ tests/test_gorest_crud.py
@@ -1,7 +1,7 @@
 import os
-
-from faker import Faker
 import requests
+from faker import Faker
+
 
 BASE_URL = "https://gorest.co.in/public/v2"
 USERS_ENDPOINT = f"{BASE_URL}/users"
@@ -9,6 +9,7 @@ USER_ENDPOINT = f"{USERS_ENDPOINT}/{{user_id}}"
 TOKEN = os.environ.get("API_TOKEN")
 
 headers = {"Authorization": f"Bearer {TOKEN}"}
+
 
 def test_list_user():
     response = requests.get(USERS_ENDPOINT)
@@ -19,22 +20,31 @@ def test_list_user():
 
     assert response.status_code == 200
 
-def test_create_user(): 
+def test_create_user():
     fake = Faker()
+    username = fake.name_male()
+    user_email = fake.email()
+    user_gender = "male"
+    user_status = "inactive"
+    
     create_user_payload = {
-        "name": fake.name_male(),
-        "email": fake.email(),
-        "gender": "male",
-        "status": "inactive"
+        "name": username,
+        "email": user_email,
+        "gender": user_gender,
+        "status": user_status
     }
 
-    response = requests.post(url= USERS_ENDPOINT, headers=headers, data=create_user_payload)
+    response = requests.post(url= USERS_ENDPOINT, headers= headers, json= create_user_payload)
     response_body = response.json()
 
     print(f"Status Code: {response.status_code}")
     print(f"Response Body: {response_body}")
 
     assert response.status_code == 201
+    assert response_body["name"] == username
+    assert response_body["email"] == user_email
+    assert response_body["gender"] == user_gender
+    assert response_body["status"] == user_status
 
 def test_get_user():
     # Create User
@@ -46,7 +56,7 @@ def test_get_user():
         "status": "inactive"
     }
 
-    create_user_response = requests.post(url= USERS_ENDPOINT, headers=headers, data=create_user_payload)
+    create_user_response = requests.post(url= USERS_ENDPOINT, headers=headers, json=create_user_payload)
     create_user_response_body = create_user_response.json()
     user_id = create_user_response_body["id"]
 
@@ -69,7 +79,7 @@ def test_put_user():
         "status": "inactive"
     }
 
-    create_user_response = requests.post(url= USERS_ENDPOINT, headers=headers, data=create_user_payload)
+    create_user_response = requests.post(url= USERS_ENDPOINT, headers=headers, json=create_user_payload)
     create_user_response_body = create_user_response.json()
     user_id = create_user_response_body["id"]
 
@@ -79,7 +89,7 @@ def test_put_user():
     update_user_payload = {
         "status": "active"
     }
-    update_user_response = requests.put(url=USER_ENDPOINT.format(user_id=user_id), headers=headers, data=update_user_payload)
+    update_user_response = requests.put(url=USER_ENDPOINT.format(user_id=user_id), headers=headers, json=update_user_payload)
     update_user_response_body = update_user_response.json()
     
     print(f"Response Status Code: {update_user_response.status_code}")
@@ -99,7 +109,7 @@ def test_delete_user():
         "status": "inactive"
     }
 
-    create_user_response = requests.post(url= USERS_ENDPOINT, headers=headers, data=create_user_payload)
+    create_user_response = requests.post(url= USERS_ENDPOINT, headers=headers, json=create_user_payload)
     create_user_response_body = create_user_response.json()
     user_id = create_user_response_body["id"]
 
